84f40d25fc533a9301f7e4b69664920528550c15
Julio Hernandez-Miyares <julio@vosdg.com>
2017-11-19 16:33:11 -0500

    changes through Sunday November 19 2017

--- wordpress/wp-content/uploads/mc4wp-debug-log.php
+++ wordpress/wp-content/uploads/mc4wp-debug-log.php
@@ -3,3 +3,6 @@
 [2017-08-24 16:21:28] WARNING: Top Bar > No MailChimp lists were selected
 [2017-10-23 16:42:20] WARNING: WPForms > juli********@ma*.com is already subscribed to the selected list(s)
 [2017-10-23 16:42:48] WARNING: Top Bar > juli********@ma*.com is already subscribed to the selected list(s)
+[2017-10-24 17:39:19] WARNING: WPForms > juli********@ma*.com is already subscribed to the selected list(s)
+[2017-10-24 17:49:24] WARNING: WPForms > juli********@ma*.com is already subscribed to the selected list(s)
+[2017-10-25 13:31:06] WARNING: WPForms > juli********@ma*.com is already subscribed to the selected list(s)
